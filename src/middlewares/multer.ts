c125cb9860c5175bdbd53701d23c05180ff0969b
Rudranayak17 <122798243+Rudranayak17@users.noreply.github.com>
2024-04-26 01:15:04 +0530

    updated Ecommerce tsconfig

--- src/middlewares/multer.ts
+++ src/middlewares/multer.ts
@@ -1,9 +1,19 @@
 import multer from "multer";
 import { v4 as uuid } from "uuid";
+import fs from "fs";
+import path from "path";
+
+// Define the uploads directory path
+const uploadsDir = path.join(__dirname, "uploads");
+
+// Ensure that the uploads directory exists, create it if it doesn't
+if (!fs.existsSync(uploadsDir)) {
+  fs.mkdirSync(uploadsDir);
+}
 
 const storage = multer.diskStorage({
   destination(req, file, callback) {
-    callback(null, "uploads");
+    callback(null, uploadsDir);
   },
   filename(req, file, callback) {
     const id = uuid();
